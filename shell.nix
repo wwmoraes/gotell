58e2745e42f2feb2f54c856d64ff2e66769f7897
William Artero <git@artero.dev>
2025-05-11 13:14:05 +0200

    chore: update nix sources
    Now using niv to manage them

--- shell.nix
+++ shell.nix
@@ -1,20 +1,14 @@
-let
-  pkgs = import (fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/refs/tags/24.05.tar.gz";
-    sha256 = "1lr1h35prqkd1mkmzriwlpvxcb34kmhc9dnr48gkm8hh089hifmx";
-  }) {
-    config.packageOverrides = pkgs: {
-      nur = import (builtins.fetchTarball "https://github.com/nix-community/NUR/archive/master.tar.gz") {
-        inherit pkgs;
-      };
-    };
-  };
-  unstable = import (fetchTarball {
-    name = "nixos-unstable-a14c5d651cee9ed70f9cd9e83f323f1e531002db";
-    url = "https://github.com/NixOS/nixpkgs/archive/a14c5d651cee9ed70f9cd9e83f323f1e531002db.tar.gz";
-    sha256 = "1b2dwbqm5vdr7rmxbj5ngrxm7sj5r725rqy60vnlirbbwks6aahb";
-  }) {};
-  inherit (pkgs) lib mkShell;
+{ system ? builtins.currentSystem
+, sources ? import ./nix/sources.nix
+}: let
+	pkgs = import sources.nixpkgs {
+		inherit system;
+		config.packageOverrides = pkgs: {
+			nur = import sources.NUR { inherit pkgs; };
+			unstable = import sources.unstable { inherit pkgs; };
+		};
+	};
+	inherit (pkgs) lib mkShell;
 in mkShell {
   packages = [
     pkgs.editorconfig-checker
@@ -25,14 +19,15 @@ in mkShell {
     pkgs.markdownlint-cli
     pkgs.nur.repos.wwmoraes.go-commitlint
     pkgs.typos
-    unstable.go
-    unstable.golangci-lint
-    unstable.sarif-fmt
+    pkgs.unstable.go
+    pkgs.unstable.golangci-lint
+    pkgs.unstable.sarif-fmt
   ] ++ lib.optionals (builtins.getEnv "CI" != "") [ # CI-only
   ] ++ lib.optionals (builtins.getEnv "CI" == "") [ # local-only
+		pkgs.niv
     pkgs.nur.repos.wwmoraes.gopium
     pkgs.nur.repos.wwmoraes.goutline
-    unstable.gopls
-    unstable.gotests
+    pkgs.unstable.gopls
+    pkgs.unstable.gotests
   ];
 }
